3b15f6e8c51d58bca62e9b878123e91261e7d46c
Shiva Verabandi <vsivasai07@gmail.com>
2025-05-26 11:50:53 +0530

    added exception to edge case for search by brand name method

---
+++ pms/src/main/java/com/telusko/titans/pms/exceptions/InvalidPageException.java
@@ -0,0 +1,7 @@
+package com.telusko.titans.pms.exceptions;
+
+public class InvalidPageException extends RuntimeException {
+  public InvalidPageException(String message) {
+    super(message);
+  }
+}
---
+++ pms/src/main/java/com/telusko/titans/pms/exceptions/PageExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.telusko.titans.pms.exceptions;
+
+public class PageExceptionHandler {
+}
