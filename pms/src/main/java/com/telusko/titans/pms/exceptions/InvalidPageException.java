cf5bfc7bfe0b9132b47e6ac280fc0032ca1c5c62
Shiva Verabandi <vsivasai07@gmail.com>
2025-05-26 12:25:25 +0530

    deleted page Exception class

--- pms/src/main/java/com/telusko/titans/pms/exceptions/InvalidPageException.java
+++
@@ -1,8 +0,0 @@
-package com.telusko.titans.pms.exceptions;
-
-
-public class InvalidPageException extends RuntimeException {
-    public InvalidPageException(String message) {
-        super(message);
-    }
-}
